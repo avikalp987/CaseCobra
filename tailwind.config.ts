14e0d185248ceaeaad6f1532c59070cba6482147
vikalp <avikalp233@gmail.com>
2024-05-19 19:20:20 +0530

    Animated landing page section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,29 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        'fade-in': {
+          from: {
+            opacity: '0',
+          },
+          to: {
+            opacity: '1',
+          },
+        },
+        marquee: {
+          '100%': {
+            transform: 'translateY(-50%)',
+          },
+        },
+        flashing: {
+          '0%, 100%': { opacity: '0.2' },
+          '20%': { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "marquee": "marquee var(--marquee-duration) linear infinite",
+        'fade-in': 'fade-in 0.5s linear forwards',
       },
     },
   },
