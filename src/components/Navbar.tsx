1f9c9d4881dd5d6498e6a6a1be7495e40348759b
vikalp <avikalp233@gmail.com>
2024-05-19 16:42:57 +0530

    Authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Navbar = () => {
-    const user = undefined
-    const isAdmin = false
+const Navbar = async () => {
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+
+    const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
     return (
         <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -32,7 +35,7 @@ const Navbar = () => {
                                 </Link>
                                 {isAdmin?
                                     <Link 
-                                        href={"/api/auth/logout"} 
+                                        href={"/dashboard"} 
                                         className={buttonVariants({
                                             size: "sm",
                                             variant: "ghost"
@@ -49,7 +52,7 @@ const Navbar = () => {
                                         className: "hidden sm:flex items-center gap-1"
                                     })}
                                     >
-                                    Create case
+                                        Create case
                                     <ArrowRight className="ml-1.5 h-5 w-5"/>
                                 </Link>
                             </>
