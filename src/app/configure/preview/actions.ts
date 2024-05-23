a257e0548a3c90bd52de6de6dd2135ea2b58d260
vikalp <avikalp233@gmail.com>
2024-05-23 14:16:15 +0530

    Creating an auth callback

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -46,6 +46,8 @@ export const createCheckoutSession = async ({configId}: {configId: string}) => {
         }
     })
 
+    console.log(user.id, configuration.id)
+
     if(existingOrder)
     {
         order = existingOrder
@@ -73,7 +75,7 @@ export const createCheckoutSession = async ({configId}: {configId: string}) => {
     const stripeSession = await stripe.checkout.sessions.create({
         success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
         cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-        payment_method_types: ["card", "amazon_pay"],
+        payment_method_types: ["card"],
         mode: "payment",
         shipping_address_collection: { allowed_countries: ["IN"] },
         metadata: {
