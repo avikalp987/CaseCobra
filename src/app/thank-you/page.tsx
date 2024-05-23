7ab1e8b92342aeb83ad4f774d8a55f29de98e952
vikalp <avikalp233@gmail.com>
2024-05-23 22:48:05 +0530

    Creating our thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import ThankYou from "./ThankYou";
+
+const Page = () => {
+    return ( 
+        <Suspense>
+
+            <ThankYou />
+
+        </Suspense>
+    );
+}
+ 
+export default Page;
